17eae84b626671dc4f0eb3c6096cb04235130034
Jordan Fest <jordan.fest1@gmail.com>
2017-07-31 15:17:26 -0500

    Final Commit

--- input/scripts/final/main.js
+++ input/scripts/final/main.js
@@ -4,12 +4,22 @@ var scene;
 
 var player;
 var gun;
+var mgBullet;
 var camera;
+
+var collisionDistance = 12.5;
 
 var enemies = [];
 var enemiesLeft = [];
 
-var targetAmount = 10;
+var currentProjectiles = [];
+
+var targetAmount = 20;
+
+var collidableMeshList = [];
+
+var arenaDemension = 20;
+var wallSection = [];
 
 rendererContainer.autoClear = false;
 
@@ -21,9 +31,14 @@ var playerControls = function()
 	this.velocity = new THREE.Vector3(0.0, 0.0, 0.0);
 	
 	this.speed = 15;
-	this.acceleration = 0.75;
+	this.acceleration = 0.5;
 	this.friction = 0.5;
-	this.rotSpeed = 0.035;
+	this.rotSpeed = 0.025;
+	
+	this.canGoForward = true;
+	this.canGoBackward = true;
+	this.canGoLeft = true;
+	this.canGoRight = true;
 	
 	this.currentWeapon;
 	
@@ -41,17 +56,18 @@ var playerControls = function()
 		var side = new THREE.Vector3(1, 0, 0);
 		side.applyProjection(matrix);
 		
+		collisionCheck(player);
+		
 		var cmd = this.command;
 		for(key in pressedKeys)
-		{
-			
-			if(key == cmd["forward"])
+		{	
+			if(key == cmd["forward"] && this.canGoForward)
 				this.addVelocity(a * dir.x, 0, a * dir.z);
-			if(key == cmd["backward"])
+			if(key == cmd["backward"] && this.canGoBackward)
 				this.addVelocity(-a * dir.x, 0, -a * dir.z);
-			if(key == cmd["strafeRight"])
+			if(key == cmd["strafeRight"] && this.canGoRight)
 				this.addVelocity(a * side.x, 0, a * side.z);
-			if(key == cmd["strafeLeft"])
+			if(key == cmd["strafeLeft"] && this.canGoLeft)
 				this.addVelocity(-a * side.x, 0, -a * side.z);
 			
 			if(key == cmd["fire"])
@@ -81,6 +97,13 @@ var playerControls = function()
 			this.velocity.z = 0.0;
 		else
 			this.velocity.z *= this.friction;
+		
+		matrix.makeRotationFromQuaternion(player.quaternion);
+		player.collisionBox.quaternion.setFromRotationMatrix(matrix);
+		
+		player.collisionBox.position.x = player.position.x;
+		player.collisionBox.position.y = player.position.y;
+		player.collisionBox.position.z = player.position.z;
 		
 	};
 	
@@ -149,10 +172,12 @@ var gunControls = function()
 	this.currentFrame;
 	
 	this.fireRate = 0.1;
-	this.ammoCapacity = 20;
-	this.ammoLoaded = 20;
+	this.ammoCapacity = 50;
+	this.ammoLoaded = 50;
 	
 	this.chambered = true;
+	
+	this.bullet;
 	
 	this.coolDownTimer;
 	
@@ -161,7 +186,7 @@ var gunControls = function()
 		if(this.ammoLoaded > 0 && this.chambered)
 		{
 			this.ammoLoaded--;
-			
+			spawnProjectile(this.bullet, player);
 			this.fireSound.play();
 			gun.controls.fireSound = new Sound(gun.controls.fireSoundSource, 1, 1);
 			
@@ -198,29 +223,71 @@ var gunControls = function()
 	}
 }
 
+var bulletControls = function()
+{
+	this.bulletSpeed = 12;
+	this.damage = 3.35;
+	
+	this.spawnOrigin;
+	this.isDeleted = false;
+	
+	this.forward = new THREE.Vector3(0.0, 0.0, -1.0);
+	
+	this.lifeTime = 2;
+	this.lifeTimer;
+
+	this.updateProjectile = function()
+	{
+		var matrix = new THREE.Matrix4();
+		matrix.extractRotation(this.spawnOrigin.matrix)
+		if(this.lifeTimer == null)
+		{
+			this.lifeTimer = new THREE.Clock();
+			this.lifeTimer.start();			
+		}
+		if(this.lifeTimer != null && this.lifeTimer.getElapsedTime() >= this.lifeTime)
+		{
+			for(var i = currentProjectiles.length - 1; i >= 0; i--)
+			{
+				if(currentProjectiles[i].uuid == this.spawnOrigin.uuid)
+				{
+					this.spawnOrigin.isDeleted = true;
+					scene.remove(this.spawnOrigin.collisionBox);
+					scene.remove(this);
+				}
+			}
+		}
+		this.spawnOrigin.position.x += this.forward.x * this.bulletSpeed;
+		this.spawnOrigin.position.y += this.forward.y * this.bulletSpeed;
+		this.spawnOrigin.position.z += this.forward.z * this.bulletSpeed;
+		this.spawnOrigin.collisionBox.position.x = this.spawnOrigin.position.x;
+		this.spawnOrigin.collisionBox.position.y = this.spawnOrigin.position.y;
+		this.spawnOrigin.collisionBox.position.z = this.spawnOrigin.position.z;
+	}
+}
+
 var enemyControls = function(name, health)
 {
 	this.name = name;
 	this.health = health;
 	this.dead = false;
+	this.isAnimating = false;
 	
-	this.id;
+	this.originPoint;
 	
-	this.positionOffet = 1.5;
+	this.positionOffset = 1.5;
 	
 	this.animationFrame = [];
 	this.currentFrame;
 	this.currentAnimationFrame = 0;
+	this.deathAnimationFrameCap = 2;
 	
-	this.deathAnimationFrameOffset = 0;
-	this.deathAnimationCount = 2;
-	
+	this.frameTime = 5;
 	this.animationTimer;
-	this.frameTime = 0.35;
-	
+
 	this.damage = function(damage)
 	{
-		(health - damage < 0) ? 0 : health - damage;
+		health = (health - damage < 0) ? 0 : health - damage;
 	}
 	
 	this.die = function()
@@ -229,10 +296,13 @@ var enemyControls = function(name, health)
 		{
 			this.dead = true;
 			
-			currentAnimationFrame++;
-			if(this.currentAnimationFrame <= this.deathAnimationCount)
+			this.currentFrame.visible = false;
+			
+			if(this.animationTimer == null)
 			{
-				animateObject(this, this.animationFrame[deathAnimationFrameOffset + currentAnimationFrame])
+				this.animationTimer = new THREE.Clock()
+				//this.animationTimer.start()
+				//this.isAnimating = true;
 			}
 		}
 	}
@@ -240,28 +310,93 @@ var enemyControls = function(name, health)
 	this.updateEnemy = function()
 	{
 		if(health <= 0)
+		{
 			this.die();
+		}
+		
+		// if(this.isAnimating && this.dead)
+		// {
+			// if(this.animationTimer.getElapsedTime() > this.frameTime)
+			// {
+				// this.animationTimer = new THREE.Clock();
+				// this.animationTimer.start();
+			// } else if(this.animationTimer.getElapsedTime() <= this.frameTime && this.currentAnimationFrame <= this.deathAnimationFrameCap){
+				// this.currentAnimationFrame++;
+				// animateObject(this, this.animationFrame[this.currentAnimationFrame]);
+			// } else {
+				// this.isAnimating = false;
+			// }
+		// }
+	}
+}
+
+function destroy(object)
+{
+	scene.remove(object);
+}
+
+function spawnProjectile(projectile, origin)
+{
+	if(projectile != null && origin != null)
+	{
+		var spawn = projectile.clone();
+		spawn.controls = new bulletControls();
+		
+		spawn.position.x = origin.position.x;
+		spawn.position.y = origin.position.y - 1;
+		spawn.position.z = origin.position.z;
+
+		var matrix = new THREE.Matrix4();
+		matrix.extractRotation(origin.matrix);
+		
+		spawn.controls.forward.applyProjection(matrix);
+		
+		scene.add(spawn);
+		
+		spawn.visible = true;
+		
+		generateCollision(spawn);
+		
+		currentProjectiles.push(spawn);
+		spawn.controls.spawnOrigin = currentProjectiles[currentProjectiles.length - 1];
 	}
 }
 
 function spawnEnemy(enemy, position)
 {
-
 	var counter = 0;
-	
+    
+    if(position.x == null) position.x = 0;
+    if(position.y == null) position.y = 0;
+    if(position.z == null) position.z = 0;
+    
 	if(enemy != null)
 	{
 		var spawn = enemy.clone();
-		spawn.controls = enemy.controls;
+		spawn.controls = new enemyControls(enemy.name, enemy.controls.health);
 		
 		spawn.position.x = position.x;
-		spawn.position.y = position.y + spawn.positionOffet;
+		spawn.position.y = position.y + spawn.controls.positionOffset;
 		spawn.position.z = position.z;
+	
+		spawn.controls.currentFrame = enemy.controls.currentFrame.clone();
+		spawn.controls.animationFrame = [];
+		for(var i = 0; i < enemy.controls.animationFrame.length; i++)
+		{
+			spawn.controls.animationFrame.push(enemy.controls.animationFrame[i].clone());
+			spawn.controls.animationFrame[spawn.controls.animationFrame.length-1].position.x = spawn.position.x;
+			spawn.controls.animationFrame[spawn.controls.animationFrame.length-1].position.y = spawn.position.y;
+			spawn.controls.animationFrame[spawn.controls.animationFrame.length-1].position.z = spawn.position.z;
+		}
 		spawn.visible = true;
 		
 		scene.add(spawn);
+        
+		generateCollision(spawn);
 		
 		enemiesLeft.push(spawn);
+		
+		spawn.controls.originPoint = spawn;
 	}
 }
 
@@ -275,6 +410,129 @@ function spawnEnemy(enemy, position)
 	}
  }
 
+ function generateCollision(object)
+ {
+	var g;
+	
+	for(var x = 0; x < object.children.length; x++)
+		if(object.children[x].name == "col")
+			g = object.children[x].geometry;
+	 
+	var colBox = new THREE.CubeGeometry(g.parameters.width, g.parameters.height, g.parameters.depth, 1, 1, 1);
+	//var colMat = new THREE.MeshBasicMaterial({color: new THREE.Color(object.material.color.r, object.material.color.g, object.material.color.b), wireframe: object.material.wireframe});
+	var colMat = new THREE.MeshBasicMaterial({color: new THREE.Color(0x0000ff), wireframe: true});
+	var colObject = new THREE.Mesh(colBox, colMat);
+	scene.add(colObject);
+	
+	object.collisionBox = colObject;
+	object.collisionBox.name = "col";
+	
+	object.collisionBox.position.x = object.position.x;
+	object.collisionBox.position.y = object.position.y;
+	object.collisionBox.position.z = object.position.z;
+	
+	//object.col.geometry.visible = true;
+	
+	collidableMeshList.push(object);
+ }
+ 
+ function collisionCheck(object)
+ {
+	var originPoint = object.collisionBox.position.clone();
+	 
+	//http://stackoverflow.com/questions/11473755/how-to-detect-collision-in-three-js
+	for (var vertexIndex = 0; vertexIndex < object.collisionBox.geometry.vertices.length; vertexIndex++)
+	{       
+		var localVertex = object.collisionBox.geometry.vertices[vertexIndex].clone();
+		var globalVertex = localVertex.applyMatrix4( object.collisionBox.matrix );
+		var directionVector = globalVertex.sub( object.collisionBox.position );
+		
+		var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+		
+		var collisionResults = ray.intersectObjects( scene.children, true );
+				
+		if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
+		{
+			//if(collisionResults[0].object.name == "gun_frame")
+				//collisionResults.pop(collisionResults[0]);
+			if(collisionResults != null && collisionResults.length > 0)
+			{
+				if(object.name == "player")
+				{
+					for(var i = 0; i < collisionResults.length; i++)
+					{
+						if(collisionResults[i].object.name == "col" && collisionResults[i].distance < collisionDistance)
+						{
+							if(collisionResults[i].point.x - object.position.x > 0.0)
+							{
+								object.controls.canGoForward = false;
+								if(object.controls.velocity.x > 0.0)
+								{
+									object.controls.velocity.x = 0.0;
+								}
+							}
+							
+							if((collisionResults[i].point.x - object.position.x < 0.0))
+							{
+								object.controls.canGoBackward = false;
+								if(object.controls.velocity.x < 0.0)
+								{
+									object.controls.velocity.x = 0.0;
+								}
+							}
+							
+							if(collisionResults[i].point.z - object.position.z > 0.0)
+							{
+								object.controls.canGoRight = false;
+								if(object.controls.velocity.z > 0.0)
+								{
+									object.controls.velocity.z = 0.0;
+								}
+							}
+							
+							if(collisionResults[i].point.z - object.position.z < 0.0)
+							{
+								object.controls.canGoLeft = false;
+								if(object.controls.velocity.z < 0.0)
+								{
+									object.controls.velocity.z = 0.0;
+								}
+							}
+						}
+					}
+				} else if (object.name == "bullet")
+				{
+					for(var x = 0; x < collisionResults.length; x++)
+					{
+						if(collisionResults[x].object.uuid != object.collisionBox.uuid && collisionResults[x].object.name == "col"  && collisionResults[x].distance < collisionDistance)
+						{
+							if(collisionResults[x].object.uuid != player.collisionBox.uuid)
+							{
+								for(var y = 0; y < enemiesLeft.length; y++)
+								{
+									if(enemiesLeft[y].collisionBox.uuid == collisionResults[x].object.uuid)
+									{
+										enemiesLeft[y].controls.damage(object.controls.damage);
+									}
+								}
+								object.controls.lifeTime = 0;
+							}
+						}
+					}
+				}
+			}
+		} else {
+			if(object.name == "player" && collisionResults.length == 0)
+			{
+				object.controls.canGoForward = true;
+				object.controls.canGoBackward = true;
+				object.controls.canGoRight = true;
+				object.controls.canGoLeft = true;
+			}
+		}
+	}
+ }
+ 
 //Scene Control
 function sceneControl(sceneNode)
 {
@@ -284,6 +542,39 @@ function sceneControl(sceneNode)
 	player.controls.updatePlayer();
 	gun.controls.updateGun();
 	
+	if(currentProjectiles != null && currentProjectiles.length > 0)
+	{
+		for(var i = 0; i < currentProjectiles.length; i++)
+		{
+			if(!currentProjectiles[i].isDeleted)
+			{
+				currentProjectiles[i].controls.updateProjectile();
+				collisionCheck(currentProjectiles[i]);
+			}
+		}
+	}
+	
+	for(var i = 0; i < enemiesLeft.length; i++)
+	{
+		enemiesLeft[i].controls.updateEnemy();
+		
+		if(enemiesLeft[i].controls.dead)
+		{
+			scene.remove(enemiesLeft[i].collisionBox);
+			enemiesLeft[i].visible = false;
+			console.log(enemiesLeft[i]);
+			animateObject(enemiesLeft[i].controls, enemiesLeft[i].controls.animationFrame[2]);
+			for(var x = 0; x < enemiesLeft[i].children; x++)
+			{
+				if(enemiesLeft[i].children[x].name == "target_frame")
+				{
+					if(enemiesLeft[i].children[x].visible == true)
+						scene.remove(enemiesLeft[i].children[x]);
+				}
+			}
+		}
+	}
+
     var loader = loader = new THREE.JSONLoader();
 }
 
@@ -316,6 +607,18 @@ function loadScene(sceneNode)
 					gun.controls.dryFireSound = new Sound(gun.controls.dryFireSoundSource, 1, 1);
 					player.controls.equipWeapon(gun);
 				}
+                if(camera == null)
+                {
+                    if(player.children[x].name == "camera")
+                    {
+                        camera = children[x];
+                        camera.far = 1000;
+                    }
+                }
+				if(player.children[x].name == "colPlayer")
+				{
+					player.col = player.children[x];
+				}
 			}
 			if(gun != null)
 			{
@@ -330,6 +633,12 @@ function loadScene(sceneNode)
 					}
 				}
 			}
+		}
+		if(gun != null && children[i].name == "bullet")
+		{
+			mgBullet = children[i];
+			mgBullet.controls = new bulletControls();
+			gun.controls.bullet = mgBullet;
 		}
 		if(children[i].name == "targetEnemy")
 		{	
@@ -349,24 +658,83 @@ function loadScene(sceneNode)
 				}
 			}
 		}
-		if(children[i].name == "camera")
+		if(children[i].name == "wall")
 		{
-			camera = children[i];
-			camera.far = 1000;
+			wallSection.push(children[i]);
+		}
+		for(var x = 0; x < children[i].children.length; x++)
+		{
+			var object = children[i].children[x];
+			
+			if(object.name == "col" && children[i] != null && children[i].name == "player")
+			{
+				generateCollision(children[i]);
+			}
 		}
 	}
 	
 	// add the listener to the camera
 	//camera.add( audioListener );
+
+	generateArena();
 	
 	populateTargets();
 	
 	//console.log("Enemy Listing");
 	//console.log(enemies);
-	console.log("Current enemies");
-	console.log(enemiesLeft);
-	
+
 	sceneLoaded = true;
+}
+
+function generateArena()
+{
+	var offset = 100;
+	var block;
+	
+	var copy = wallSection[0];
+	
+	console.log("Generating Arena");
+	for(var x = offset; x >= -offset;)
+	{
+		copy.position.z = offset;
+		
+		copy.position.x = x;
+		block = wallSection[0].clone();
+		generateCollision(block);
+		scene.add(block);
+		
+		copy.position.z = -offset;
+		
+		copy.position.x = -x;
+		block = wallSection[0].clone();
+		generateCollision(block);
+		scene.add(block);
+		
+		x -= 10;
+	}
+	
+	copy.position.x = offset;
+	
+	for(var z = offset; z >= -offset;)
+	{
+		copy.position.x = offset;
+		
+		copy.position.z = z;
+		block = wallSection[0].clone();
+		generateCollision(block);
+		scene.add(block);
+		
+		copy.position.x = -offset;
+		
+		copy.position.z = -z;
+		block = wallSection[0].clone();
+		generateCollision(block);
+		scene.add(block);
+		
+		z -= 10;
+	}
+	
+	console.log("Done");
 }
 
 function populateTargets()
@@ -376,11 +744,9 @@ function populateTargets()
 		if(enemies[enemy] == enemies["Target"])
 		{
 			for(var i = 0; i < targetAmount; i++)
-				spawnEnemy(enemies[enemy], new THREE.Vector3((Math.random() * 20) + 1, 0, (Math.random() * 20) + 1));
+				spawnEnemy(enemies[enemy], new THREE.Vector3((Math.random() * 200) - 99, 0.0, (Math.random() * 200) - 99));
 		}
 	}
-	
-	console.log("Done");
 }
 
 //https://threejsdoc.appspot.com/doc/three.js/examples.source/misc_sound.html.html
--- input/sprint2scenes/scene1fscript.js
+++
@@ -1,9 +0,0 @@
-
-function rotateScript(sceneNode)
-{
-    // Constants
-    var rotationSpeed = sceneNode.userData["rotationSpeed"];
-    var frameRate = 60.0;
-    sceneNode.rotateOnAxis(YAXIS, rotationSpeed/frameRate);
-}
-
